1ce769e85a96c7e6f3b899df3ca666efd7df7536
tech sysvbo <ccbnadoad-erwdd@gmail.com>
2026-04-28 17:03:09 -0400

    QF TF file

--- terraform/demo-s3/versions.tf
+++ terraform/demo-s3/versions.tf
@@ -2,13 +2,13 @@ terraform {
   #required_version = ">= 1.5.0"
 
   #required_providers {
-   # aws = {
-   #   source  = "hashicorp/aws"
-   #   version = "~> 5.0"
-   # }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.6"
-   # }
+  # aws = {
+  #   source  = "hashicorp/aws"
+  #   version = "~> 5.0"
+  # }
+  random = {
+    source  = "hashicorp/random"
+    version = "~> 3.6"
+    # }
   }
 }
