2b1614f99fa60b0167bebc6aecaefbd5c1918ee1
tech sysvbo <ccbnadoad-erwdd@gmail.com>
2026-07-06 17:59:09 -0400

    for demo

---
+++ terraform/environments/prod-s3-demo/main.tf
@@ -0,0 +1,38 @@
+resource "aws_s3_bucket" "demo" {
+  bucket = var.bucket_name
+}
+
+resource "aws_s3_bucket_public_access_block" "demo" {
+  bucket = aws_s3_bucket.demo.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_ownership_controls" "demo" {
+  bucket = aws_s3_bucket.demo.id
+
+  rule {
+    object_ownership = "BucketOwnerEnforced"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "demo" {
+  bucket = aws_s3_bucket.demo.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "demo" {
+  bucket = aws_s3_bucket.demo.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
