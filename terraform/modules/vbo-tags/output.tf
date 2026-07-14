c42cd29c3e7e9c73985f41b47ffc95a4f9aca8f3
tech sysvbo <ccbnadoad-erwdd@gmail.com>
2026-07-14 16:33:39 -0400

    add new govcloud

---
+++ terraform/modules/vbo-tags/output.tf
@@ -0,0 +1,4 @@
+output "tags" {
+  description = "Standard VBO tag map applied to all taggable resources."
+  value       = local.vbo_tags
+}
---
+++ terraform/modules/vbo-tags/variables.tf
@@ -0,0 +1,10 @@
+locals {
+  vbo_tags = {
+    "vbo:app:name"    = "abc"
+    "vbo:cost:bu"     = "costa"
+    "vbo:ops:team"    = "eng"
+    "vbo:app:owner"   = "adams smith"
+    "vbo:auto:backup" = "delete30"
+    "vbo:auto:env"    = "prod"
+  }
+}
